7bcf5d81b460a34a2bdd279004b567bffcd83b74
kurui <kuruijoshua@gmail.com>
2025-08-25 10:13:04 +0300

    Implement relationship methods: Author.add_book, Publisher.publish_book, Book links

--- lib/challenge.py
+++ lib/challenge.py
@@ -3,17 +3,34 @@
 class Author:
     def __init__(self, name):
         self.name = name
-        self.books = []  # will hold books written by this author
+        self.books = []
+
+    def add_book(self, book):
+        """Associate a book with this author."""
+        if book not in self.books:
+            self.books.append(book)
+            book.author = self  # set the book's author reference
 
 
 class Book:
     def __init__(self, title, author=None, publisher=None):
         self.title = title
-        self.author = author
-        self.publisher = publisher
+        self.author = None
+        self.publisher = None
+
+        if author:
+            author.add_book(self)   # link to author if passed
+        if publisher:
+            publisher.publish_book(self)  # link to publisher if passed
 
 
 class Publisher:
     def __init__(self, name):
         self.name = name
-        self.books = []  # will hold books published by this publisher
+        self.books = []
+
+    def publish_book(self, book):
+        """Associate a book with this publisher."""
+        if book not in self.books:
+            self.books.append(book)
+            book.publisher = self  # set the book's publisher reference
