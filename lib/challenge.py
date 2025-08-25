5ed8d0a5855f068281ab1226fc0dac02d4029b40
kurui <kuruijoshua@gmail.com>
2025-08-25 10:12:32 +0300

    Define __init__ methods and attributes for Author, Book, Publisher

--- lib/challenge.py
+++ lib/challenge.py
@@ -1,13 +1,19 @@
 # lib/challenge.py
 
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.books = []  # will hold books written by this author
 
 
 class Book:
-    pass
+    def __init__(self, title, author=None, publisher=None):
+        self.title = title
+        self.author = author
+        self.publisher = publisher
 
 
 class Publisher:
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        self.books = []  # will hold books published by this publisher
